88b0adf57a6a2ec573c376be0412a7635c2b2dd9
Reuben James <20reuben.james@utcportsmouth.org>
2024-03-07 21:46:34 +0000

    Create error messages

---
+++ errors.py
@@ -0,0 +1,33 @@
+class Error(Exception):
+    """Base class for all exceptions within the module."""
+
+    def __init__(self,
+                 error_code: str,
+                 sql_state: str,
+                 error_message: str
+                 ):
+        self._message = f"SQL Error [{error_code}] ({sql_state}): {error_message}"
+
+    def __repr__(self):
+        return self._message
+
+    def __str__(self):
+        return self._message
+
+class DataError(Error):
+    pass
+
+class DatabaseError(Error):
+    pass
+
+class IntegrityError(Error):
+    pass
+
+class InternalError(Error):
+    pass
+
+class ProgrammingError(Error):
+    pass
+
+class Warning(Error):
+    pass
